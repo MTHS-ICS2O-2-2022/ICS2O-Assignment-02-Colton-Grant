4215e619bbd628666de300098fe5a57fe956412b
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-03-28 14:50:13 +0000

    added favicon

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function enterClicked() {
+  // input
+  const radius = parseInt(document.getElementById('radius-of-circle').value)
+  const pi= parseInt(document.getElementById('3.14').value)
+
+  // process
+  const area = pi * radius * radius
+
+  // output
+  document.getElementById('area').innerHTML = 
+    'Area is: ' + (pi * radius * radius) + ' cm²'
+}
